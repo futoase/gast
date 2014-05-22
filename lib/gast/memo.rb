79a29083d70f15f3d8d5ac41b430939854f19c65
futoase <futoase@gmail.com>
2014-05-22 09:19:31 +0900

    Modified: Ignore nothing commit.

--- lib/gast/memo.rb
+++ lib/gast/memo.rb
@@ -13,6 +13,7 @@ module Gast
     end
 
     def self.update(id, content)
+      return id if content.to_s.chomp == item(id).chomp
       @repo = Gast::Repository.new(id)
       @repo.content = content.to_s
       @repo.publish
